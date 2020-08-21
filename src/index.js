297584600198f4302105362876b75807fd6047b6
Julian Camilo <julian20141016@gmail.com>
2020-08-21 15:49:38 -0500

    Add CodeSanbox base

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+//IMPORTANTE
+//Link base para estudiar contextAPI
+//https://codesandbox.io/s/todoconcontext-ie87f?file=/src/context/TodoContext.js&fbclid=IwAR1gK0waALUijUY-0f58bZ59tJJ1Bdq7wZmhFsITPeCs-sLOnlNvoH54Wx4
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
